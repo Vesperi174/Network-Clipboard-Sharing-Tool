a3701b820747cb55473a637c0c088859e73bfe79
Vesperi <1023625028@qq.com>
2026-06-23 17:12:14 +0800

    集成FlatLaf 3.7.1暗色主题，DeepSeek风格界面美化

--- src/com/clipboard/client/GUIClipboardClient.java
+++ src/com/clipboard/client/GUIClipboardClient.java
@@ -699,9 +699,9 @@ public class GUIClipboardClient extends JFrame {
         refreshButton.setEnabled(connected);
 
         if (connected) {
-            statusLabel.setForeground(Color.GREEN.darker());
+            statusLabel.setForeground(new Color(100, 255, 100));
         } else {
-            statusLabel.setForeground(Color.RED);
+            statusLabel.setForeground(new Color(255, 100, 100));
         }
     }
 
@@ -709,10 +709,13 @@ public class GUIClipboardClient extends JFrame {
         SimpleLogger.init("clipboard_client_" + System.currentTimeMillis() + ".log");
         SwingUtilities.invokeLater(() -> {
             try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                com.formdev.flatlaf.FlatDarkLaf.setup();
+                UIManager.put("Button.arc", 8);
+                UIManager.put("Component.arc", 8);
+                UIManager.put("TextComponent.arc", 8);
             } catch (Exception e) {
                 try {
-                    UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
